74021c997a999f201b18600fa2ffa58e828aa8fe
Shivam <s.padaliya@efhutton.com>
2018-07-01 23:13:56 -0400

    added dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,29 @@
 module.exports = {
     siteMetadata: {
-        title: '',
-        description: 'Shivam Padaliya | Software Engineer',
+        title: 'Shivam Padaliya | Software Engineer',
+        description: 'MY personal Website',
         site_url: '',
         author: 'Shivam Padaliya'
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass',
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    'open sans\:400,700'
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "",
+            }
+        },
+        {
+            resolve: `gatsby-plugin-sitemap`
+        }
+    ],
 };
