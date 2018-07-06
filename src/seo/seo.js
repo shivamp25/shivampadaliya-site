70cb14a4fb0684b11f56587c305bb61083e847ac
Shivam <s.padaliya@efhutton.com>
2018-07-06 14:19:56 -0400

    SEO

---
+++ src/seo/seo.js
@@ -0,0 +1,69 @@
+export const siteMeta = {
+    title: 'Shivam Padaliya | Software Engineer',
+    favicon: 'https://',
+    og: {
+        description: "Hello! I'm Shivam Padaliya",
+        title: "Shivam Padaliya | Software Engineer",
+        site_name: "Shivam Padaliya | Software Engineer",
+        type: "website",
+        url: "https://shivampadaliya.com",
+        image: {
+            url: "https://",
+            alt: "Shivam Padaliya | Software Engineer",
+            type: "image/jpeg",
+            width: "400",
+            height: "400",
+        },
+        locale: {
+            main: "en_US",
+        }
+    },
+    author: "Shivam Padaliya",
+    copyright: "Shivam Padaliya, Copyright (c) 2018",
+    keywords: [
+        "Shivam Padaliya",
+        "frontend developer",
+        "frontend engineer",
+        "fullstack engineer",
+        "web developer",
+        "software engineer",
+        "software developer",
+        "engineer",
+
+    ],
+};
+
+export const aboutMe = {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "Shivam Padaliya",
+    "jobTitle": "Software Engineer",
+    "url": "https://shivampadaliya.com",
+    "sameAs": [
+        "https://github.com/shivampadaliya25",
+        "https://www.linkedin.com/in/shivampadaliya",
+    ],
+    "worksFor": {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "url": "https://efhutton.com/"
+    },
+    "workLocation": {
+        "@type": "City",
+        "name": "Columbus"
+    },
+    "gender": "male",
+    "image": "https://shivampadaliya.com/images/me.jpeg",
+    "alumniOf": "Texas A&M University"
+};
+
+export const aboutWebsite = {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "url": "https://shivampadaliya.com",
+    "name": "Shivam Padaliya | Software Engineer",
+    "author": {
+        "@type": "Person",
+        "name": "Shivam Padaliya"
+    }
+};
